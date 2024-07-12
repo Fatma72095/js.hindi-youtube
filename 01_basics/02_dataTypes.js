f93f14ffb127316aa562fdf17a7be9d9c570c6b4
Fatma72095 <fatmazahidkhan65@gmail.com>
2024-07-12 08:01:20 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code in new version 
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3)// code readabiltiy should be high
+
+console.log("Fatma")
+
+
+let name = "Fatma"
+let age = 18
+let isloggedin = false
+let state; 
+
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Fatma");
+
+console.log(typeof age);
+
+console.log(typeof null); // object
